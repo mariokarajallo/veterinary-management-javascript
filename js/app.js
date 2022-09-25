831a6ecbdc982ec9117fdc2314a7a80f19cb8a47
mariokarajallo <mario_karajallo@hotmail.com>
2022-09-24 22:11:46 -0400

    feat: definicion selectores del HTML

---
+++ js/app.js
@@ -0,0 +1,9 @@
+const mascotaInput = document.querySelector("#mascota");
+const propietarioInput = document.querySelector("#propietario");
+const telefonoInput = document.querySelector("#telefono");
+const fechaInput = document.querySelector("#fecha");
+const horaInput = document.querySelector("#hora");
+const sintomasInput = document.querySelector("#sintomas");
+
+const formulario = document.querySelector("#nueva-cita");
+const contenedorCitas = document.querySelector("#citas");
