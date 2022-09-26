99c25701a164ce82d350cb4eb763635592098118
mariokarajallo <mario_karajallo@hotmail.com>
2022-09-26 14:51:45 -0400

    feat: se agrego el boton editar en el HTML para el listado de citas, el boton llama a la funcion cargarEdicion()

--- js/app.js
+++ js/app.js
@@ -121,6 +121,16 @@ class UI {
 </svg>`;
       btnEliminar.onclick = () => eliminarCita(id);
 
+      // agregar un boton para editar
+      const btnEditar = document.createElement("button");
+      btnEditar.classList.add("btn", "btn-info");
+      btnEditar.innerHTML = `Editar <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125" />
+</svg>
+`;
+      //accion al darle click, pasamos el objeto completo de cita
+      btnEditar.onclick = () => cargarEdicion(cita);
+
       //agregar los parrafos al DIV
       divCita.appendChild(mascotaParrafo);
       divCita.appendChild(propietarioParrafo);
@@ -129,6 +139,7 @@ class UI {
       divCita.appendChild(horaParrafo);
       divCita.appendChild(sintomasParrafo);
       divCita.appendChild(btnEliminar);
+      divCita.appendChild(btnEditar);
 
       //agregar las citas al HTML
       contenedorCitas.appendChild(divCita);
