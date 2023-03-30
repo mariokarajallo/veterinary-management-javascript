2ff7ecc9012b6a9ecb1ac45c30229175f14b767f
mariokarajallo <mariokarajallo92@gmail.com>
2023-03-30 17:46:26 -0400

    refactor: create selectores.js

--- js/app.js
+++ js/app.js
@@ -1,16 +1,3 @@
-//!VARIABLES
-//?campos del formulario
-const mascotaInput = document.querySelector("#mascota");
-const propietarioInput = document.querySelector("#propietario");
-const telefonoInput = document.querySelector("#telefono");
-const fechaInput = document.querySelector("#fecha");
-const horaInput = document.querySelector("#hora");
-const sintomasInput = document.querySelector("#sintomas");
-
-//?UI : interfaz del usuario
-const formulario = document.querySelector("#nueva-cita");
-const contenedorCitas = document.querySelector("#citas");
-
 // objeto donde se guardaran los datos del paciente/CITA
 const citaObj = {
   mascotas: "",
---
+++ js/selectores.js
@@ -0,0 +1,12 @@
+//!VARIABLES
+//?campos del formulario
+const mascotaInput = document.querySelector("#mascota");
+const propietarioInput = document.querySelector("#propietario");
+const telefonoInput = document.querySelector("#telefono");
+const fechaInput = document.querySelector("#fecha");
+const horaInput = document.querySelector("#hora");
+const sintomasInput = document.querySelector("#sintomas");
+
+//?UI : interfaz del usuario
+const formulario = document.querySelector("#nueva-cita");
+const contenedorCitas = document.querySelector("#citas");
